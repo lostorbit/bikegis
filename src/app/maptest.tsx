78b7ec1d8afb8f1968e855ae280fe6b6f8c739cb
Steve Massey <stevenrmassey@gmail.com>
2023-06-19 13:16:55 -0700

    working to add dynamic geojson loading

--- src/app/maptest.tsx
+++ src/app/maptest.tsx
@@ -1,25 +1,52 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-import { LatLngTuple } from 'leaflet';
+// React
+import { useEffect, useState } from 'react';
 
+// Leaflet
+import { MapContainer, TileLayer, useMap, Marker, Popup, GeoJSON } from 'react-leaflet'
+import { LatLngTuple } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+export default async function MapTest() {
 
-export default function MapTest() {
+    const [cdMap, setCdMap] = useState<GeoJSON.FeatureCollection>();
 
-    const position = [51.505, -0.09] as LatLngTuple;
+    //
+    // Dynamically load a map from somewhere (beacuse I can't get the direct file import to work lol)
+    const cd_location = "/src/assets/cd.geojson";
+    // const cd_map_remote = await fetch(cd_location)
+    // .then(function(response) {
+    //   return response.json();
+    // }) as GeoJSON.FeatureCollection;
 
-    return(
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+    // Dump what we got
+    // console.log("cd_map", cd_map_remote)
 
-        <Marker position={position}>
-            <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-        </Marker>
-        </MapContainer>
+    // Set the map
+    useEffect(() => {
+        async function getMapData(loc: string) {
+            const cd_map_remote = await fetch(loc)
+                .then(function(response) {
+                    return response.json();
+                }) as GeoJSON.FeatureCollection;
+            setCdMap(cd_map_remote);
+        }
+
+        getMapData(cd_location);
+        
+    }, [cd_location]);
+
+    // Set a default center location
+    const mapCenter = [34.0982107,-118.2658979] as LatLngTuple;
+
+    return (
+        <>            
+            <MapContainer center={mapCenter} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+                <GeoJSON data={cdMap} />
+            </MapContainer>
+        </>
     )
 };
