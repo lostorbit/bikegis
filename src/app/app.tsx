333dd04e4964c7e7d5693c034f3967b3996f8b61
Steve Massey <stevenrmassey@gmail.com>
2023-06-19 12:09:01 -0700

    inital setup of nx and leaflet

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
+import MapTest from './maptest';
+import Container from '@mui/material/Container';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="bikegis" />
+    <Container>
+        <MapTest />
+        </Container>
     </div>
   );
 }
