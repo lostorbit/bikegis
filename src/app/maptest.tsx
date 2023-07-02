aa234392b3eb0b9b95123e369d73af8aa9fc82ba
Steve Massey <stevenrmassey@gmail.com>
2023-07-02 12:29:57 -0700

    loading geojson from file!

--- src/app/maptest.tsx
+++ src/app/maptest.tsx
@@ -2,14 +2,18 @@
 import { useEffect, useState } from 'react';
 
 // Leaflet
-import { MapContainer, TileLayer, useMap, Marker, Popup, GeoJSON } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup, GeoJSON, LayerGroup } from 'react-leaflet'
+// import { GeoJsonTypes } from 'geojson';
+import * as geojson from 'geojson';
 import { LatLngTuple } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-export default async function MapTest() {
+import hash from 'object-hash';
 
-    const [cdMap, setCdMap] = useState<GeoJSON.FeatureCollection>();
+export default function MapTest() {
 
+    const [cdMap, setCdMap] = useState<geojson.GeoJsonObject>({type: "Point", coordinates: [0,0]} as geojson.GeoJsonObject);
+        
     //
     // Dynamically load a map from somewhere (beacuse I can't get the direct file import to work lol)
     const cd_location = "/src/assets/cd.geojson";
@@ -23,30 +27,37 @@ export default async function MapTest() {
 
     // Set the map
     useEffect(() => {
-        async function getMapData(loc: string) {
+
+        const getMapData = async(loc: string) => {
             const cd_map_remote = await fetch(loc)
                 .then(function(response) {
                     return response.json();
-                }) as GeoJSON.FeatureCollection;
+                }) as geojson.FeatureCollection;
+            
+            // set the map
             setCdMap(cd_map_remote);
+            console.log("cd_map", cd_map_remote)
         }
 
-        getMapData(cd_location);
-        
-    }, [cd_location]);
+        getMapData(cd_location).catch(console.error);
+
+    }, []);
+
+
+
 
     // Set a default center location
     const mapCenter = [34.0982107,-118.2658979] as LatLngTuple;
 
-    return (
-        <>            
-            <MapContainer center={mapCenter} zoom={13} scrollWheelZoom={false}>
+    return (       
+        <MapContainer center={mapCenter} zoom={13} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-                <GeoJSON data={cdMap} />
-            </MapContainer>
-        </>
+            <LayerGroup>
+                <GeoJSON key={hash(cdMap)} data={cdMap} />
+            </LayerGroup>
+        </MapContainer>
     )
 };
